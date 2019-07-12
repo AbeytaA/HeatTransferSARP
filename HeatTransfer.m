08302057aed450879d6ebabdd2c5aa3a14ad47ef
Aaron Abeyta <41973830+AbeytaA@users.noreply.github.com>
2019-07-12 16:50:27 -0700

    Add files via upload

--- HeatTransfer.m
+++ HeatTransfer.m
@@ -1,6 +1,21 @@
 % Heat Transfer Model 
+clear all; close all; clc;
 
-
-
-
-pi = 3.14
+%%%%%%%%%% Definition of Variables %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% q = heat transfer per unit area per unit time (W/m^2)
+% Twl = temperature of inner wall on liquid side (K)
+% Tl = temperature of liquid (K)
+% Twg = temperature of inner wall on gas side (K)
+% hg = heat transfer coefficient of gas 
+% hl = heat transfer coefficient of liquid
+% tw = wall thickness (m)
+% t = time (s)
+% mu = viscosity 
+% Pr = prandtl number
+% cp = heat capacity 
+% k = thermal conductivity 
+% Dc = Chamber Diameter (m)
+% Dt = Throat diameter (m)
+% Dh = hydraulic diameter (m)
+% rho = density (kg/m^3)
+% v = velocity (m/s)
